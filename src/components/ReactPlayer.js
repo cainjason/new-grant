fa2c21e9cee5f09a2a30cb2d1cb0c169b00ecf8f
Jason Cain <118203097+cainjason@users.noreply.github.com>
2023-04-26 17:00:59 -0400

    chore: ReactPlayer.js Component

---
+++ src/components/ReactPlayer.js
@@ -0,0 +1,11 @@
+import ReactPlayer from "react-player";
+
+const ReactPlayer = () => {
+    return (
+        <>
+            {<ReactPlayer url='https://fb.watch/k5blMm1Ac9/' />}
+        </>
+    );
+}
+
+export default ReactPlayer;
